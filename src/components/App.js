6791e962ebe7e1c47922d91e53cdb6e22ae7d150
moringa <oscarokumu47@gmail.com>
2022-09-25 12:34:26 +0300

    Done with core Deliverables

--- src/components/App.js
+++ src/components/App.js
@@ -10,11 +10,14 @@ function App() {
   function handleDarkModeClick() {
     setIsDarkMode((isDarkMode) => !isDarkMode);
   }
+  function handleAddOfItems (newItem){
+setItems([...items,newItem])
+  }
 
   return (
     <div className={"App " + (isDarkMode ? "dark" : "light")}>
       <Header isDarkMode={isDarkMode} onDarkModeClick={handleDarkModeClick} />
-      <ShoppingList items={items} />
+      <ShoppingList items={items} myNewItem = {handleAddOfItems} />
     </div>
   );
 }
